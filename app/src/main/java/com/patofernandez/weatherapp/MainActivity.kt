e6891845151e555234be57cbe5cfb6e6c8f2848d
patriciofernandez <patricio.fernandez@wolox.com.ar>
2020-05-28 21:50:24 -0300

    initial commit

--- app/src/main/java/com/patofernandez/weatherapp/MainActivity.kt
+++ app/src/main/java/com/patofernandez/weatherapp/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.patofernandez.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.patofernandez.weatherapp.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.patofernandez.weatherapp.viewmodel.WeatherViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
     }
 }
